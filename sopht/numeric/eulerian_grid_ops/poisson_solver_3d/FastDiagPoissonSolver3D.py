8f27f82482b8ebb5382c67194739c3c34420e85a
Bhosale <bhosale2@illinois.edu>
2022-10-30 12:37:30 -0500

    update: remove transposed mem alloc in fast diag solver

--- sopht/numeric/eulerian_grid_ops/poisson_solver_3d/FastDiagPoissonSolver3D.py
+++ sopht/numeric/eulerian_grid_ops/poisson_solver_3d/FastDiagPoissonSolver3D.py
@@ -141,12 +141,9 @@ class FastDiagPoissonSolver3D:
             rhs_field, self.inv_of_eig_vecs_x, axes=(2, 1)
         )
         # hit middle y index
-        self.spectral_field_buffer[...] = np.transpose(
-            np.tensordot(
-                self.inv_of_eig_vecs_y, self.spectral_field_buffer, axes=(1, 1)
-            ),
-            axes=(1, 0, 2),
-        )
+        self.spectral_field_buffer[...] = np.tensordot(
+            self.inv_of_eig_vecs_y, self.spectral_field_buffer, axes=(1, 1)
+        ).transpose((1, 0, 2))
         # hit first z index
         self.spectral_field_buffer[...] = np.tensordot(
             self.inv_of_eig_vecs_z, self.spectral_field_buffer, axes=(1, 0)
@@ -165,10 +162,9 @@ class FastDiagPoissonSolver3D:
             self.spectral_field_buffer, self.eig_vecs_x, axes=(2, 1)
         )
         # hit middle y index
-        self.spectral_field_buffer[...] = np.transpose(
-            np.tensordot(self.eig_vecs_y, self.spectral_field_buffer, axes=(1, 1)),
-            axes=(1, 0, 2),
-        )
+        self.spectral_field_buffer[...] = np.tensordot(
+            self.eig_vecs_y, self.spectral_field_buffer, axes=(1, 1)
+        ).transpose((1, 0, 2))
         # hit first z index
         solution_field[...] = np.tensordot(
             self.eig_vecs_z, self.spectral_field_buffer, axes=(1, 0)
